e226aff6166d710445df387b9db7e6b66fb3ad10
Paweł Satora <pawel@Pawel.home>
2024-12-03 19:27:46 +0100

    Remove unused imports

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,4 +1,4 @@
-import { Box, Paper } from "@mui/material"
+import { Box } from "@mui/material"
 import { LoginPanel } from "../components/LoginPanel/LoginPanel"
 import { ToastContainer } from "react-toastify"
 
